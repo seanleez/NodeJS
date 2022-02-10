e18d7a0336dbf85b95bdb8f8efd66dce7d8ad589
Van Vien Dong <vanviendong96@gmail.com>
2022-02-10 16:35:37 +0700

    LAB01_9

--- routes.js
+++ routes.js
@@ -4,7 +4,6 @@ const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
     if (url === '/') {
-        // res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write(
