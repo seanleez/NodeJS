6e5f87eb91fa58b26a1cc03357e6c7e17295498d
Van Vien Dong <vanviendong96@gmail.com>
2022-02-18 22:51:35 +0700

    LAB04_17

--- models/product.js
+++ models/product.js
@@ -47,6 +47,16 @@ module.exports = class Product {
         });
     }
 
+    static deleteById(id) {
+        getProductsFromFile((products) => {
+            const updatedProduct = products.filter((prod) => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
+                if (!err) {
+                }
+            });
+        });
+    }
+
     static fetchAll(cb) {
         getProductsFromFile(cb);
     }
