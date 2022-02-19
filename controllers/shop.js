069f04d95fe752b209f250541dccd4fbf81f2f27
Van Vien Dong <vanviendong96@gmail.com>
2022-02-19 11:33:19 +0700

    LAB04_19

--- controllers/shop.js
+++ controllers/shop.js
@@ -32,9 +32,26 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
+    Cart.getCart((cart) => {
+        Product.fetchAll((products) => {
+            const cartProduct = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(
+                    (prod) => prod.id === product.id
+                );
+                if (cartProductData) {
+                    cartProduct.push({
+                        productData: product,
+                        qty: cartProductData.qty,
+                    });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts,
+            });
+        });
     });
 };
 
