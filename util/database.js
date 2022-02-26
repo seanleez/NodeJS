d056dfae8a8b1a62f655e0f56d63800fd7b8a3cd
Van Vien Dong <vanviendong96@gmail.com>
2022-02-26 23:10:16 +0700

    LAB05_1

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'thaonhinheo0706',
+});
+
+module.exports = pool.promise();
