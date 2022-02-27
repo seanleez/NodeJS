12f66f5020406b810f974126de0181210514184b
Van Vien Dong <vanviendong96@gmail.com>
2022-02-27 10:33:50 +0700

    LAB05_11

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,17 +10,20 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
     const price = req.body.price;
+    const imageUrl = req.body.imageUrl;
     const description = req.body.description;
-    const product = new Product(null, title, imageUrl, description, price);
-    product
-        .save()
-        .then(() => {
-            res.redirect('/');
+    Product.create({
+        title: title,
+        price: price,
+        imageUrl: imageUrl,
+        description: description,
+    })
+        .then((result) => {
+            console.log(result);
+            console.log('Created Product');
         })
         .catch((err) => console.log(err));
-    res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
