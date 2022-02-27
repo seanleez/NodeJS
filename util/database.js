6424097e4865d10cf9600eeed85ced3986ca0558
Van Vien Dong <vanviendong96@gmail.com>
2022-02-27 09:41:23 +0700

    LAB05_8

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'thaonhinheo0706', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'thaonhinheo0706',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
