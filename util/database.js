6fa21a34d5a11e7cb59df1eb214f202b1c1468b3
Van Vien Dong <vanviendong96@gmail.com>
2022-03-03 23:42:34 +0700

    LAB06_3

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
     MongoClient.connect(
         'mongodb+srv://admin:thaonhinheo0706@cluster0.5qg3p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     )
         .then((client) => {
             console.log('Connected!');
-            callback(client);
+            _db = client.db();
+            callback();
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.log(err);
+            throw err;
+        });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
