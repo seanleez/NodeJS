28183381a7e94431d2094a8119f37ebaaccfd9e7
Van Vien Dong <vanviendong96@gmail.com>
2022-03-03 23:01:36 +0700

    LAB06_1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'thaonhinheo0706', {
-    dialect: 'mysql',
-    host: 'localhost',
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://admin:thaonhinheo0706@cluster0.5qg3p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+        .then((client) => {
+            console.log('Connected!');
+            callback(client);
+        })
+        .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
