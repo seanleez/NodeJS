fbb5c861b06b118b02439323c4435b93d905ce45
Van Vien Dong <vanviendong96@gmail.com>
2022-03-05 16:07:07 +0700

    LAB06_16

--- models/user.js
+++ models/user.js
@@ -4,14 +4,28 @@ const mongodb = require('mongodb');
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this);
+    }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.item.findIndex((cp) => {
+        //     return cp._id === product.id;
+        // });
+        const updatedCart = { items: [{ ...product, quantity: 1 }] };
+        const db = getDb();
+        db.collection('users').updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: updatedCart } }
+        );
     }
 
     static findById(userId) {
