1c4bfaf84045bb7ae90845d69cf0555f7a4a6114
Van Vien Dong <vanviendong96@gmail.com>
2022-03-06 00:55:06 +0700

    LAB06_20

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,7 +69,10 @@ exports.postCart = (req, res, next) => {
         .then((product) => {
             return req.user.addToCart(product);
         })
-        .then((result) => console.log(result));
+        .then((result) => {
+            console.log(result);
+            res.redirect('/cart');
+        });
     // let fetchedCart;
     // let newQuantity = 1;
     // req.user
