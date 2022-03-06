f4c33e4822a57c1272220d253f5bb89c2536c3f0
Van Vien Dong <vanviendong96@gmail.com>
2022-03-06 15:45:33 +0700

    LAB07_16

--- controllers/shop.js
+++ controllers/shop.js
@@ -97,8 +97,9 @@ exports.postOrder = (req, res, next) => {
             return order.save();
         })
         .then((result) => {
-            res.redirect('/orders');
+            return req.user.clearCart();
         })
+        .then(() => res.redirect('/orders'))
         .catch((err) => console.log(err));
 };
 
