5a4770113a19825ac6073dc32cf2ac9d803b65d7
Van Vien Dong <vanviendong96@gmail.com>
2022-03-06 15:40:25 +0700

    LAB07_15

--- controllers/shop.js
+++ controllers/shop.js
@@ -85,7 +85,10 @@ exports.postOrder = (req, res, next) => {
         .execPopulate()
         .then((user) => {
             const products = user.cart.items.map((i) => {
-                return { quantity: i.quantity, product: i.productId };
+                return {
+                    quantity: i.quantity,
+                    product: { ...i.productId._doc },
+                };
             });
             const order = new Order({
                 user: { name: req.user.name, userId: req.user },
