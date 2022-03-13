a4d289368cbe807eeea2d085bb823e5b27525b41
Van Vien Dong <vanviendong96@gmail.com>
2022-03-13 22:26:21 +0700

    LAB08_8

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    // console.log(req.get('Cookie').trim().split('=')[1]);
-    // const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
     console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
@@ -10,6 +10,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('6224578b6d3b3c3aad17b4f4')
+        .then((user) => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
