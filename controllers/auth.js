b4b80dafb15a190d7434c835efde06726eecce5a
Van Vien Dong <vanviendong96@gmail.com>
2022-03-13 21:24:53 +0700

    LAB08_6

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 exports.getLogin = (req, res, next) => {
     // console.log(req.get('Cookie').trim().split('=')[1]);
     // const isLoggedIn = req.get('Cookie').trim().split('=')[1] === 'true';
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -9,6 +10,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; Secure');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
