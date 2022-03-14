222981b6d3132be49a046ea42ebc405f28d1d570
Van Vien Dong <vanviendong96@gmail.com>
2022-03-14 22:48:20 +0700

    LAB08_17

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,13 +19,23 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('6224578b6d3b3c3aad17b4f4')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
         .then((user) => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect('/');
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcrypt.compare(password, user.password).then((doMatch) => {
+                if (doMatch) {
+                    req.session.isLoggedIn = true;
+                    req.session.user = user;
+                    return req.session.save((err) => {
+                        console.log(err);
+                        res.redirect('/');
+                    });
+                }
+                res.redirect('/login');
             });
         })
         .catch((err) => console.log(err));
@@ -54,7 +64,6 @@ exports.postSignup = (req, res, next) => {
                     res.redirect('/login');
                 });
         })
-
         .catch((err) => console.log(err));
 };
 
