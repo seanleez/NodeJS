8350c5446ddfb227b0e81b5c8b1980635b909cef
Van Vien Dong <vanviendong96@gmail.com>
2022-03-15 12:07:20 +0700

    LAB09_4

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -14,15 +14,23 @@ router.post('/login', authController.postLogin);
 
 router.post(
     '/signup',
-    check('email')
-        .isEmail()
-        .withMessage('Please enter a valid email!')
-        .custom((value, { req }) => {
-            if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden');
-            }
-            return true;
-        }),
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email!')
+            .custom((value, { req }) => {
+                if (value === 'test@test.com') {
+                    throw new Error('This email address is forbidden');
+                }
+                return true;
+            }),
+        body(
+            'password',
+            'Please enter a password with only numbers and text and at least 5 characters'
+        )
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+    ],
     authController.postSignup
 );
 
