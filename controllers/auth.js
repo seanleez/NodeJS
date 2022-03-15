eb42e1f0218a01024ebccb89bcd19cd3ea64f247
Van Vien Dong <vanviendong96@gmail.com>
2022-03-15 12:30:40 +0700

    LAB09_6

--- controllers/auth.js
+++ controllers/auth.js
@@ -72,7 +72,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log(errors.array());
@@ -82,28 +81,18 @@ exports.postSignup = (req, res, next) => {
             errorMessage: errors.array()[0].msg,
         });
     }
-    User.findOne({ email: email })
-        .then((userDoc) => {
-            if (userDoc) {
-                req.flash(
-                    'error',
-                    'Email exists already, please pick a different one.'
-                );
-                return res.redirect('/signup');
-            }
-            return bcrypt
-                .hash(password, 12)
-                .then((hashedPassword) => {
-                    const user = new User({
-                        email: email,
-                        password: hashedPassword,
-                        cart: { items: [] },
-                    });
-                    return user.save();
-                })
-                .then((result) => {
-                    res.redirect('/login');
-                });
+    bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+            const user = new User({
+                email: email,
+                password: hashedPassword,
+                cart: { items: [] },
+            });
+            return user.save();
+        })
+        .then((result) => {
+            res.redirect('/login');
         })
         .catch((err) => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -19,10 +20,17 @@ router.post(
             .isEmail()
             .withMessage('Please enter a valid email!')
             .custom((value, { req }) => {
-                if (value === 'test@test.com') {
-                    throw new Error('This email address is forbidden');
-                }
-                return true;
+                // if (value === 'test@test.com') {
+                //     throw new Error('This email address is forbidden');
+                // }
+                // return true;
+                return User.findOne({ email: value }).then((userDoc) => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'Email exists already, please pick a different one'
+                        );
+                    }
+                });
             }),
         body(
             'password',
