e00964295abe8336a0f902ef12bd49597492a253
Van Vien Dong <vanviendong96@gmail.com>
2022-03-15 11:55:58 +0700

    LAB09_3

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,15 @@ router.post('/login', authController.postLogin);
 
 router.post(
     '/signup',
-    check('email').isEmail().withMessage('Please enter a valid email!'),
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email!')
+        .custom((value, { req }) => {
+            if (value === 'test@test.com') {
+                throw new Error('This email address is forbidden');
+            }
+            return true;
+        }),
     authController.postSignup
 );
 
