97d680a48546eb16411d2797ce8984b040a3d0bd
Van Vien Dong <vanviendong96@gmail.com>
2022-03-16 11:43:38 +0700

    LAB10_3

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,9 +20,10 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.image;
+    const imageUrl = req.file;
     const price = req.body.price;
     const description = req.body.description;
+    console.log(imageUrl);
     const errors = validationResult(req);
 
     // status 422 inform browser that some data input isn't correct
